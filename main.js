225226a3c379cb4f18ac5a00382b0422cbbdc556
Tomati <dopedup@mail.ru>
2020-08-06 12:16:20 +0300

    AdaptiveImage and main.js changes create an illusion of adaptive width of image

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-function FirstJs(){
-    let texts = document.getElementById("text");
-    if (texts.classList.contains("animate")){
-        texts.classList.remove("animate");
-        console.log("class was removed");
-    }
-    
-    texts.classList.add("animate");
-    console.log("class was added");
-}
+let img = document.getElementById("AdaptiveImage");
+let container = document.getElementsByClassName("image");
+
+console.log(img.width)
+console.log(img.height)
+
+const image_h = img.height;
+const image_w = img.width;
+const container_w = container[0].clientWidth;
+let new_image_h = (image_h / image_w) * container_w;
+
+let cws = container_w.toString() + "px";
+console.log(cws);
+
+
+img.style.width = cws;
+//img.style.height = new_image_h.toString();
+
+console.log(img.width)
+console.log(img.height)
