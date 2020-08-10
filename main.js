967f854fb53074a026025a557115ad04d5bba4bb
Tomati <dopedup@mail.ru>
2020-08-10 14:34:44 +0300

    That it

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let img = document.getElementById("AdaptiveImage");
 let container = document.getElementsByClassName("image");
 
-console.log(img.width)
-console.log(img.height)
+console.log(img.width);
+console.log(img.height);
 
 const image_h = img.height;
 const image_w = img.width;
@@ -16,5 +16,5 @@ console.log(cws);
 img.style.width = cws;
 //img.style.height = new_image_h.toString();
 
-console.log(img.width)
-console.log(img.height)
+console.log(img.width);
+console.log(img.height);
