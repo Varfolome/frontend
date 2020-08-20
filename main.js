52c9828672f498ea889aa5e35b5a13b745f0e08e
Tomati <dopedup@mail.ru>
2020-08-20 23:25:10 +0300

    Started work on desktop version. Basic position management. First slider

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
 //dont touch now
+
+'use strict';
+
+$(document).ready(function(){
+   
+    const width = 350;
+    let current = 1;
+    const animationSpeed = 2000;
+    const pause = 5500;
+    
+    
+    var $carousel = $("#animate");
+    var $container = $("#carousel");
+    var $slides = $(".carousel-item", $container);
+    
+    var interval;
+    
+    function startCarousel() {
+        interval = setInterval(function(){
+            $container.animate({'margin-left' : '-='+width},animationSpeed,function(){
+                if(++current === $slides.length) {
+                    current = 1;
+                    $container.animate({'margin-left' : '0'}, 4000, function(){});
+                    //$container.css('margin-left',0);
+                }
+            });
+        },pause);
+    }
+    
+    function pauseInterval() {
+        clearInterval(interval);
+    }
+    
+    $container.on('mouseenter', pauseInterval).on('mouseleave', startCarousel);
+    
+    startCarousel();
+    
+});
