df42a333abc22457eadc35b51fa590aee98ecefb
Tomati <dopedup@mail.ru>
2020-08-23 23:12:26 +0300

    Padding was changed

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req,res) => {
   res.json({
-    message: 'forntend project server'
+    message: 'frontend project server'
   });
 });
 
