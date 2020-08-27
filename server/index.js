69ab4dc9bf0e450b49e772a6886567724269f766
Tomati <dopedup@mail.ru>
2020-08-28 01:01:36 +0300

    Database was added. SendMailer was added. Not tested yet. Yung Lean's Warlord is the best album ever

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
+const nodemailer = require('nodemailer');
 
 const uri = "mongodb+srv://tsimafei:dataformeower123@cluster0.muwtv.mongodb.net/contacts?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {useNewUrlParser: true});
@@ -9,6 +10,21 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+function isValidName(contact) {
+  return contact.lastName && contact.lastName.toString().trim() !== ''
+  && contact.firstName && contact.firstName.toString().trim() !== '';
+}
+
+function isValidMail(contact) {
+  let mail = contact.mail.toString();
+  for(var i = (mail.length-1); i>0; i--){
+    if(mail[i] === '@'){
+      return true;
+    }
+  }
+  return false;
+}
 
 client.connect(err => {
 
@@ -21,7 +37,47 @@ client.connect(err => {
   });
 
   app.post('/contacts', (req,res) => {
-    
+    if(isValidName(req.body) && isValidMail(req.body)) {
+      let lastNameString = req.body.lastName.toString();
+      let firstNameString = req.body.firstName.toString();
+      let mailString = req.body.mail.toString();
+
+      let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: '',
+          password: ''
+        }
+      });
+
+      let mailOptions = {
+        from: '',
+        to: '',
+        subject: '',
+        text: ''
+      };
+
+      transporter.sendMail(mailOptions, function(err,info){
+        if(err){
+          console.log(err);
+        } else {
+          console.log(info);
+        }
+      });
+
+      const contact = {
+        lastname: lastNameString,
+        firstname: firstNameString,
+        mail: mailString,
+        created: new Date()
+      };
+      contacts.insertOne(contact);
+    } else {
+      res.status(422);
+      res.json({
+        message: 'We have a problem with you data!'
+      });
+    }
   });
 
 
