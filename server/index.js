918d619d91dbe0c943c42b2684284e2ec60e9e1f
Tomati <dopedup@mail.ru>
2020-08-27 15:46:03 +0300

    Started working on database and smtp

--- server/index.js
+++ server/index.js
@@ -1,22 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = "mongodb+srv://tsimafei:dataformeower123@cluster0.muwtv.mongodb.net/contacts?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {useNewUrlParser: true});
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res) => {
-  res.json({
-    message: 'frontend project server'
+client.connect(err => {
+
+  const contacts = client.db('contacts').collection('contacts');
+
+  app.get('/', (req,res) => {
+    res.json({
+      message: 'frontend project server'
+    });
   });
-});
 
-app.post('/contacts', (req,res) => {
-  console.log(req.body);
-});
+  app.post('/contacts', (req,res) => {
+    
+  });
 
 
-app.listen(5000, () => {
-  console.log('Listening');
+  app.listen(5000, () => {
+    console.log('Listening');
+  });
+
 });
